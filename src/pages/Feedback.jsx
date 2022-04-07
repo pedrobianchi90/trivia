3072f3b5ce3210d9121add941084d3a2d3bc56cd
Kaique Coelho <kaiquescoelho@hotmail.com>
2022-04-07 19:36:57 -0300

    adiciona feedback dos pontos e perguntas acertadas requisito 14

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -5,6 +5,28 @@ import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
+  componentDidMount() {
+    this.setLocal();
+  }
+
+  setLocal = () => {
+    const { rootState: { player: { correctAnswers, score } } } = this.props;
+    const player = {
+      correctAnswers,
+      score,
+    };
+    const local = JSON.parse(localStorage.getItem('player'));
+    if (local) {
+      const lista = [...local, player];
+      const localString = JSON.stringify(lista);
+      localStorage.setItem('player', localString);
+    } else {
+      const lista = [player];
+      const localString = JSON.stringify(lista);
+      localStorage.setItem('player', localString);
+    }
+  }
+
   handleResults = () => {
     const { rootState: { player: { correctAnswers } } } = this.props;
     const average = 3;
@@ -13,15 +35,26 @@ class Feedback extends React.Component {
   }
 
   render() {
+    const { rootState: { player: { correctAnswers, score } } } = this.props;
     return (
       <>
         <Header />
         <h1>Feedback</h1>
         <h2 data-testid="feedback-text">{ this.handleResults() }</h2>
+        <p data-testid="feedback-total-question">
+          { correctAnswers }
+        </p>
+        { score > 0 ? (
+          <p data-testid="feedback-total-score">
+            { score }
+          </p>
+        ) : (
+          <p data-testid="feedback-total-score">
+            0
+          </p>) }
         <Link to="/">
           <button type="button" data-testid="btn-play-again">Play Again</button>
         </Link>
-
       </>
     );
   }
